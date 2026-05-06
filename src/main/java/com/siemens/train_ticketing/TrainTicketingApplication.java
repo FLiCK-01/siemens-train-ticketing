ca1e1f15547dc9cf4750ac2dc948d14109f6ff86
mihai <cosmagiumihai1@outlook.com>
2026-05-06 16:46:28 +0300

    initial spring boot project setup

---
+++ src/main/java/com/siemens/train_ticketing/TrainTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.siemens.train_ticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainTicketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainTicketingApplication.class, args);
+	}
+
+}
